4dab3c1cf4c8d3b9646d830a1f5462c111c19b73
Johnkl725 <john.castillo1@unmsm.edu.pe>
2025-02-26 11:45:39 -0500

    ajustando para movil

--- my-dashboard/src/App.tsx
+++ my-dashboard/src/App.tsx
@@ -8,24 +8,24 @@ const App: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>('hechos')
 
   return (
-    <div className="container mx-auto p-8 bg-gray-100 min-h-screen">
+    <div className="container mx-auto p-6 bg-gray-100 min-h-screen">
       <h1 className="text-4xl font-semibold text-center text-blue-600 mb-8">Dashboard</h1>
 
       {/* Botones de navegación para cambiar de tabla */}
-      <div className="flex justify-center mb-8">
+      <div className="flex flex-wrap justify-center mb-8 gap-4">
         <button
           onClick={() => setActiveTab('hechos')}
-          className={`px-6 py-2 rounded-md font-semibold text-white mr-4 ${
+          className={`px-6 py-2 rounded-md font-semibold text-white ${
             activeTab === 'hechos' ? 'bg-blue-500' : 'bg-gray-400'
-          }`}
+          } transition-colors duration-300`}
         >
           Hechos
         </button>
         <button
           onClick={() => setActiveTab('imagenes')}
-          className={`px-6 py-2 rounded-md font-semibold text-white mr-4 ${
+          className={`px-6 py-2 rounded-md font-semibold text-white ${
             activeTab === 'imagenes' ? 'bg-blue-500' : 'bg-gray-400'
-          }`}
+          } transition-colors duration-300`}
         >
           Imágenes
         </button>
@@ -33,7 +33,7 @@ const App: React.FC = () => {
           onClick={() => setActiveTab('usuarios')}
           className={`px-6 py-2 rounded-md font-semibold text-white ${
             activeTab === 'usuarios' ? 'bg-blue-500' : 'bg-gray-400'
-          }`}
+          } transition-colors duration-300`}
         >
           Usuarios
         </button>
