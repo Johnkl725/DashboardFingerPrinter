891a3bf61ea0a06d90241a7a0424c9855e006fc3
Johnkl725 <john.castillo1@unmsm.edu.pe>
2025-02-26 10:11:23 -0500

    cambiooo

--- serverDashboard/src/routes.js
+++ serverDashboard/src/routes.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,61 +17,61 @@ const express_1 = __importDefault(require("express"));
 const database_1 = __importDefault(require("./models/database"));
 const router = express_1.default.Router();
 // Obtener todos los usuarios
-router.get('/usuarios', async (req, res) => {
+router.get('/usuarios', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const result = await database_1.default.query('SELECT * FROM usuarios');
+        const result = yield database_1.default.query('SELECT * FROM usuarios');
         res.status(200).json(result.rows);
     }
     catch (error) {
         console.error('Error al obtener usuarios:', error);
         res.status(500).json({ error: 'Error al obtener usuarios' });
     }
-});
+}));
 // Obtener todas las imágenes
-router.get('/imagenes', async (req, res) => {
+router.get('/imagenes', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const result = await database_1.default.query('SELECT * FROM userimg');
+        const result = yield database_1.default.query('SELECT * FROM userimg');
         res.status(200).json(result.rows);
     }
     catch (error) {
         console.error('Error al obtener imágenes:', error);
         res.status(500).json({ error: 'Error al obtener imágenes' });
     }
-});
+}));
 // Obtener todos los hechos
-router.get('/hechos', async (req, res) => {
+router.get('/hechos', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const result = await database_1.default.query('SELECT * FROM hechos');
+        const result = yield database_1.default.query('SELECT * FROM hechos');
         res.status(200).json(result.rows);
     }
     catch (error) {
         console.error('Error al obtener hechos:', error);
         res.status(500).json({ error: 'Error al obtener hechos' });
     }
-});
+}));
 // Crear un nuevo hecho
-router.post('/hechos', async (req, res) => {
+router.post('/hechos', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id_usuario, estado } = req.body;
     const fecha = new Date();
     try {
-        const result = await database_1.default.query('INSERT INTO hechos(id_usuario, fecha, estado) VALUES($1, $2, $3) RETURNING *', [id_usuario, fecha, estado]);
+        const result = yield database_1.default.query('INSERT INTO hechos(id_usuario, fecha, estado) VALUES($1, $2, $3) RETURNING *', [id_usuario, fecha, estado]);
         res.status(201).json(result.rows[0]);
     }
     catch (error) {
         console.error('Error al crear un hecho:', error);
         res.status(500).json({ error: 'Error al crear un hecho' });
     }
-});
+}));
 // Registrar un nuevo usuario
-router.post('/usuarios', async (req, res) => {
+router.post('/usuarios', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { id_usuario, nombre, id_huella } = req.body;
     try {
-        const result = await database_1.default.query('INSERT INTO usuarios(id_usuario, nombre, id_huella) VALUES($1, $2, $3) RETURNING *', [id_usuario, nombre, id_huella]);
+        const result = yield database_1.default.query('INSERT INTO usuarios(id_usuario, nombre, id_huella) VALUES($1, $2, $3) RETURNING *', [id_usuario, nombre, id_huella]);
         res.status(201).json(result.rows[0]);
     }
     catch (error) {
         console.error('Error al registrar un usuario:', error);
         res.status(500).json({ error: 'Error al registrar un usuario' });
     }
-});
+}));
 exports.default = router;
